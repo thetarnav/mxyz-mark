b7a75202c4206c4939903701b8bf3f644a35b7df
Damian Tarnawski <gthetarnav@gmail.com>
2023-05-06 00:41:11 +0200

    Prepare playground for developing maze generation

--- apps/solid-web/src/routes/index/(home).tsx
+++ apps/solid-web/src/routes/index/(home).tsx
@@ -1,3 +1,5 @@
+import { Index, createSignal, untrack } from 'solid-js'
+
 const randomInt = (max: number) => Math.floor(Math.random() * max)
 
 const DIRECTIONS = [
@@ -53,34 +55,106 @@ function generateNoise(width: number, height: number) {
   return result
 }
 
+function generateMaze(width: number, height: number) {
+  const length = width * height
+  const result = Array.from({ length }, () => ({
+    right: false,
+    down: false,
+  }))
+
+  for (let i = 0; i < length; i++) {
+    const cell = result[i]
+    const x = i % width
+    const y = Math.floor(i / width)
+
+    console.log(x, y)
+  }
+
+  return result
+}
+
 export default function Home() {
   const W = 20
   const H = 10
   return (
     <main>
       <h4>Noise</h4>
-      <div
-        style={{
-          display: 'grid',
-          'grid-template-columns': `repeat(${W}, 1fr)`,
-          'grid-template-rows': `repeat(${H}, 1fr)`,
-          width: `${W * 2}rem`,
-          height: `${H * 2}rem`,
-        }}
-      >
-        {generateNoise(W, H).map((cell, i) => (
-          <div
-            style={{
-              background: cell ? '#000' : 'gray',
-              display: 'flex',
-              'align-items': 'center',
-              'justify-content': 'center',
-            }}
-          >
-            {i}
-          </div>
-        ))}
-      </div>
+      {untrack(() => {
+        const [track, trigger] = createSignal(undefined, { equals: false })
+
+        return (
+          <>
+            <button onClick={() => trigger()}>Regenerate</button>
+            <br />
+            <br />
+            <div
+              style={{
+                display: 'grid',
+                'grid-template-columns': `repeat(${W}, 1fr)`,
+                'grid-template-rows': `repeat(${H}, 1fr)`,
+                width: `${W * 2}rem`,
+                height: `${H * 2}rem`,
+                border: '1px solid #fff',
+              }}
+            >
+              <Index each={(track(), generateNoise(W, H))}>
+                {(cell, i) => (
+                  <div
+                    style={{
+                      background: cell() ? 'gray' : 'transparent',
+                      display: 'flex',
+                      'align-items': 'center',
+                      'justify-content': 'center',
+                    }}
+                  >
+                    {i}
+                  </div>
+                )}
+              </Index>
+            </div>
+          </>
+        )
+      })}
+      <h4>Maze</h4>
+      {untrack(() => {
+        const [track, trigger] = createSignal(undefined, { equals: false })
+
+        return (
+          <>
+            <button onClick={() => trigger()}>Regenerate</button>
+            <br />
+            <br />
+            <div
+              style={{
+                display: 'grid',
+                'grid-template-columns': `repeat(${W}, 1fr)`,
+                'grid-template-rows': `repeat(${H}, 1fr)`,
+                width: `${W * 2}rem`,
+                height: `${H * 2}rem`,
+                border: '1px solid #fff',
+              }}
+            >
+              <Index each={(track(), generateMaze(W, H))}>
+                {(cell, i) => (
+                  <div
+                    style={{
+                      background: 'transparent',
+                      display: 'flex',
+                      'align-items': 'center',
+                      'justify-content': 'center',
+                      border: '0 solid #fff',
+                      'border-right-width': cell().right ? '1px' : '0',
+                      'border-bottom-width': cell().down ? '1px' : '0',
+                    }}
+                  >
+                    {i}
+                  </div>
+                )}
+              </Index>
+            </div>
+          </>
+        )
+      })}
     </main>
   )
 }
