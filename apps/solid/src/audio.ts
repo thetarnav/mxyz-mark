cb50193523135031086b0d11542f2a26eaa869b0
Damian Tarnawski <gthetarnav@gmail.com>
2023-07-06 21:52:09 +0200

    Store sounds in data folder

--- apps/solid/src/audio.ts
+++ apps/solid/src/audio.ts
@@ -1,16 +1,8 @@
 import { STEP_INTERVAL } from './held_direction'
 import { math } from './lib'
+import SOUNDS from '../../../data/sounds.json'
 
 const ECHO_INTERVAL = 150
-
-const STEP_AUDIO_PATHS = [
-    // 'hallway_step_1.mp3',
-    // 'hallway_step_2.mp3',
-    'hallway_step_3.mp3',
-    'hallway_step_4.mp3',
-    'hallway_step_5.mp3',
-    'hallway_step_6.mp3',
-]
 
 let step_playing = false
 let last_step_path: string
@@ -18,7 +10,7 @@ let last_step_path: string
 export function queueStepAudio() {
     if (step_playing) return
 
-    last_step_path = math.pickRandomExclidingOne(STEP_AUDIO_PATHS, last_step_path)
+    last_step_path = math.pickRandomExclidingOne(SOUNDS.step, last_step_path)
     const audio = new Audio(last_step_path)
     audio.volume = math.randomFromTo(0.1, 0.2)
     audio.playbackRate = math.randomFromTo(0.9, 1.1)
